2efe8fa3a686d8da71254590b85256fac7eb6f48
Paulo Campos <paulovitorwd@gmail.com>
2025-05-13 12:27:01 -0300

    feat: adding close button

---
+++ src/app/components/close/close.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-close',
+  templateUrl: './close.component.html',
+  styleUrl: './close.component.scss',
+  standalone: true
+})
+export class CloseComponent {}
--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -11,6 +11,7 @@ import { characterNormalizer } from '@utils/character-normalizer.utils';
 import { ItemService } from '@services/item.service';
 import { HeaderComponent } from '@components/header/header.component';
 import { SearchComponent } from '@components/search/search.component';
+import { CloseComponent } from '@components/close/close.component';
 import { CardComponent } from '@components/card/card.component';
 
 @Component({
@@ -22,6 +23,7 @@ import { CardComponent } from '@components/card/card.component';
     CommonModule,
     HeaderComponent,
     SearchComponent,
+    CloseComponent,
     CardComponent
   ]
 })
@@ -38,11 +40,20 @@ export class HomeComponent implements OnInit {
   }
 
   onSearch(value: string) {
-    this.filteredList = this.loadedList.filter(({ title }) => {
+    this.filteredList = this.loadedList.filter(({ title, description }) => {
       const titleNormalized = characterNormalizer(title);
+      const descriptionNormalized = characterNormalizer(description);
       const valueNormalized = characterNormalizer(value);
 
-      return !value.length || titleNormalized.includes(valueNormalized);
+      return (
+        !value.length ||
+        titleNormalized.includes(valueNormalized) ||
+        descriptionNormalized.includes(valueNormalized)
+      );
     });
+  }
+
+  removeItem(id: number) {
+    console.log(`Remove: ${id}`);
   }
 }
