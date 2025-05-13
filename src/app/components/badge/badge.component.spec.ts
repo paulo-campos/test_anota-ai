4de47c0f800eaee5fac2c3c6356d6e70562c3552
Paulo Campos <paulovitorwd@gmail.com>
2025-05-13 17:44:47 -0300

    test: adding badge test

--- src/app/components/badge/badge.component.spec.ts
+++ src/app/components/badge/badge.component.spec.ts
@@ -1,4 +1,8 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  TestBed
+} from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { BadgeComponent } from './badge.component';
 
@@ -17,7 +21,37 @@ describe('BadgeComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('start', () => {
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
+  });
+
+  describe('[color]', () => {
+    it('should show the color entered in the background', () => {
+      const color = 'black';
+
+      component.color = color;
+      fixture.detectChanges();
+
+      const badge = fixture.debugElement.query(By.css('.badge'));
+      const badgeBackgroundColor = badge.nativeElement.style.backgroundColor;
+
+      expect(badgeBackgroundColor).toEqual(color);
+    });
+  });
+
+  describe('[label]', () => {
+    it('should show the text entered in the label', () => {
+      const label = 'label test';
+
+      component.label = label;
+      fixture.detectChanges();
+
+      const badge = fixture.debugElement.query(By.css('.badge'));
+      const badgeTextContent = badge.nativeElement?.textContent;
+
+      expect(badgeTextContent).toContain(label);
+    });
   });
 });
--- src/app/components/badge/badge.component.ts
+++ src/app/components/badge/badge.component.ts
@@ -14,6 +14,6 @@ import {
   ]
 })
 export class BadgeComponent {
-  @Input() label = '';
   @Input() color = '';
+  @Input() label = '';
 }
